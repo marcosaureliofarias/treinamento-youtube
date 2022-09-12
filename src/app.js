fd431c04f12bc7346057e7a51e00d220d4a44d81
unknown <marcosaureliofarias2023@gmail.com>
2022-09-12 16:41:18 -0300

    rotas

--- src/app.js
+++ src/app.js
@@ -17,9 +17,18 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
   livros.push(req.body);
   res.status(201).send("Livro cadastrado com sucesso");
 });
+
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
 
 export default app;
