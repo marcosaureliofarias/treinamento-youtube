0c1e5b8324d9a5780ce1d3fcfd1ca5900e6b255a
unknown <marcosaureliofarias2023@gmail.com>
2022-09-12 15:43:31 -0300

    rotas

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http");
-const port = 3000;
+import app from "./src/app.js";
 
-const rotas = {
-  "/": "API-Nodejs",
-  "/livros": "Pagina de livros",
-  "/autores": "pagina de autores",
-  "/animes": "Pagina de animes",
-};
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plan" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const livros = [
+  { id: 1, titulo: "Senho dos Aneis" },
+  { id: 2, titulo: "O Hobbit" },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("API-NODEJS");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso");
+});
+
+export default app;
