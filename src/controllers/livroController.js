0a3261f852a5eed08baab784b5647c967883e297
unknown <marcosaureliofarias2023@gmail.com>
2022-09-13 13:42:16 -0300

    controller

---
+++ src/controllers/livroController.js
@@ -0,0 +1,11 @@
+import livros from "../model/Livro.js";
+
+class LivroController {
+  static listarLivros = (req, res) => {
+    livros.find((err, livros) => {
+      res.status(200).json(livros);
+    });
+  };
+}
+
+export default LivroController;
