a4066b7027af0af19d9e571880ec35d3321462f0
unknown <marcosaureliofarias2023@gmail.com>
2022-09-13 14:06:00 -0300

    metodo post

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,6 @@ import LivroController from "../controllers/livroController.js";
 const router = express.Router();
 
 router.get("/livros", LivroController.listarLivros);
+router.post("/livros", LivroController.cadastrarLivro);
 
 export default router;
